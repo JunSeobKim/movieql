7324b777e61f52c7f1f3a702b63f45f80c06cc76
MainDesktop_KimJunSeob <wnstjq616@gmail.com>
2019-10-04 23:59:42 +0900

    movie

---
+++ graphql/db.js
@@ -0,0 +1,26 @@
+export const people = [
+  {
+    id: "0",
+    name: "Jun",
+    age: 16,
+    gender: "male"
+  },
+  {
+    id: "1",
+    name: "asd",
+    age: 16,
+    gender: "male"
+  },
+  {
+    id: "2",
+    name: "zsgds",
+    age: 73,
+    gender: "smale"
+  },
+  {
+    id: "3",
+    name: "jhtr",
+    age: 48,
+    gender: "faga"
+  }
+];
